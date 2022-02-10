07459900fa57f29663cd3d7a54d5e8da231ff37c
salva villa <salwy2704@gmail.com>
2022-02-10 17:04:30 -0300

    arreglo cagada

--- Saldrul_Ejercicios/Ejercicios_1/Ejercicio_1_1_Mejorado.py
+++ Saldrul_Ejercicios/Ejercicios_1/Ejercicio_1_1_Mejorado.py
@@ -2,16 +2,13 @@
 # multiplique el primero por el segundo y le sume el tercero.
 # Presente el resultado en la terminal
 def calcu(x: int, y: int, z: int) -> int:
-    return(x * y) + z
+    return (x * y) + z
 
 
 print("A continuación coloque 3 numeros, el 1ero será multiplicado por el 2do y luego el 3ero"
-          "será sumado a la cuenta")
+      "será sumado a la cuenta")
 valor_1 = int(input("Primer Número: "))
 valor_2 = int(input("Segundo Número: "))
 valor_3 = int(input("Tercer Número: "))
 
-
-print(calcu(valor_1,valor_2,valor_3))
-
-
+print(calcu(valor_1, valor_2, valor_3))
