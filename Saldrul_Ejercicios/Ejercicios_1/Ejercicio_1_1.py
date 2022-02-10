de7d38f189796bd47ab6aa6cd6e6f600b61875c5
salva villa <salwy2704@gmail.com>
2022-02-10 15:23:05 -0300

    Este es el primer commit de Saldrul

---
+++ Saldrul_Ejercicios/Ejercicios_1/Ejercicio_1_1.py
@@ -0,0 +1,12 @@
+# Cree un programa que tome tres valores por consola
+# multiplique el primero por el segundoy le sume el tercero.
+# Presente el resultado en la terminal
+
+print("A continuación coloque 3 numeros, el 1ero será multiplicado por el 2do y luego el 3ero"
+      "será sumado a la cuenta")
+valor_1 = int(input())
+valor_2 = int(input())
+valor_3 = int(input())
+
+resultado = valor_1 * valor_2 + valor_3
+print(resultado)
---
+++ Saldrul_Ejercicios/Ejercicios_1/Ejercicio_1_2.py
@@ -0,0 +1,6 @@
+# Cree un programa que incorpore el módulo sys, al cual desde la terminal se le puedan
+# pasar tres parámetros. El programa debe tomar los parámetros e indicar en la terminal si
+# son múltiplos de dos.
+import sys
+
+
