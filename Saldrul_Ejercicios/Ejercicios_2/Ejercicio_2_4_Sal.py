cfd8d59ae69aab04a08efee56203a2bdc2f98209
salva villa <salwy2704@gmail.com>
2022-02-12 16:06:02 -0300

    Mejore el ejercicio 2_4

--- Saldrul_Ejercicios/Ejercicios_2/Ejercicio_2_4_Sal.py
+++ Saldrul_Ejercicios/Ejercicios_2/Ejercicio_2_4_Sal.py
@@ -1,15 +1,16 @@
-indicador_genero = input("¿Cuál es tu género? por favor indiquelo con una "
-                         "M mayuscula (Masculino) o una F mayuscula (Femenino) ")
+i_g = str(input("¿Cuál es tu género? por favor indiquelo con una "
+                "M (Masculino) o una F (Femenino) "))
 edad_jubilatoria_M = 65
 edad_jubilatoria_F = 60
-
-if indicador_genero == "M":
+i_g = i_g.lower()
+print(i_g)
+if i_g == "m" or i_g == "hombre" or i_g == "masculino":
     hombre = int(input("¿ M Cuál es tu edad? "))
     if hombre < edad_jubilatoria_M:
         print("Aún está en edad de trabajar")
     elif hombre >= edad_jubilatoria_M:
         print("Ya esá en edad de jubilarse")
-else:
+if i_g == "f" or i_g == "mujer" or i_g == "femenino":
     mujer = int(input("¿ F Cuál es tu edad? "))
     if mujer < edad_jubilatoria_F:
         print("Aún está en edad de trabajar")
