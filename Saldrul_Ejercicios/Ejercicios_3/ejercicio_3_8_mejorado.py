7d0e947a34a4751b47bdeac2d0d98b2749a1f77d
salva villa <salwy2704@gmail.com>
2022-03-10 01:46:15 -0300

    Ejercicio 8 unidad 3 good

--- Saldrul_Ejercicios/Ejercicios_3/ejercicio_3_8_mejorado.py
+++ Saldrul_Ejercicios/Ejercicios_3/ejercicio_3_8_mejorado.py
@@ -6,7 +6,7 @@ class Verdu:
     def comprarCosas(self):
         while (self.loop) == True:
             resp = input("Comrar: C, Remover compra: R, Mirar compra: M, Terminar: T "
-                         "Organizar compra: O").upper()
+                         "Organizar compra: O ").upper()
             if resp == "C":
                 self.comprar()
             elif resp == "R":
@@ -22,15 +22,20 @@ class Verdu:
         print("Llevate esta... sheesh!!!")
 
     def comprar(self):
+        cual = "Compra numero:"
+        iden = 0
         self.lista_compras = []
         que = input("¿Qué producto vas a comprar? ").capitalize()
         cuanto = int(input("¿Cuánto vas a comprar? "))
         precio = int(input("Precio del producto: "))
-        total = cuanto * precio
+        total = f"total:, ${cuanto * precio}"
+        self.lista_compras.append(cual)
+        self.lista_compras.append(iden)
         self.lista_compras.append(que)
         self.lista_compras.append(cuanto)
         self.lista_compras.append(precio)
         self.lista_compras.append(total)
+        iden += 1
         self.compras_cliente.append(self.lista_compras)
         print("Compraste ", self.lista_compras)
 
@@ -55,15 +60,25 @@ class Verdu:
 
     def organizar(self):
         cambiar = int(input("Para cambiar una compra pone la posición en la que se encuentra tu producto, "
-                          "posición primera = 1: "))
+                            "posición primera = 0: "))
         if cambiar > len(self.compras_cliente):
             print("---" * 34)
-            print("Tu posición no es una compra realizada")
+            print("Tu posición no es una compra realizada ")
             print("---" * 34)
-        nombre = input("Si quieres cambiar el nombre escribe la modificación,"
-                       " si no, escribe un número")
-        if nombre ==    
-        self.compras_cliente[cambiar] = nombre
+        whating = int(input("¿Qué cosa querés cambiar? nombre = 1, cantidad = 2, precio = 3 "))
+        if whating == 1:
+            nombre = input("Pone el nuevo nombre ")
+            self.compras_cliente[cambiar][2] = nombre
+        elif whating == 2:
+            cantidad = int(input("Pone la nueva cantidad "))
+            self.compras_cliente[cambiar][3] = cantidad
+            actualizar = f"total:, {self.compras_cliente[cambiar][3] * self.compras_cliente[cambiar][4]}"
+            self.compras_cliente[cambiar][5] = actualizar
+        elif whating == 3:
+            precio = int(input("Pone el nuevo precio "))
+            self.compras_cliente[cambiar][4] = precio
+            actualizar = f"total:, ${self.compras_cliente[cambiar][3] * self.compras_cliente[cambiar][4]}"
+            self.compras_cliente[cambiar][5] = actualizar
         print("Organizaste")
 
 
